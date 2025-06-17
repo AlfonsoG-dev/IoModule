b2f2966e97918b0c0d1a5828a23fd8f469cb7962
AlfonsoG-dev <alfgomachi@gmail.com>
2025-06-16 22:55:33 -0500

    feat(scope): `FutureTask` implementation
    1. Using the `callable` method to `listFiles` we obtain a list with the
       files inside a random directory.
    2. With `FutureTask` we invoke the callable method and obtain
       asynchronously the files inside a directory.

--- src/application/IoModule.java
+++ src/application/IoModule.java
@@ -1,12 +1,28 @@
+
+import java.util.List;
 import java.io.File;
+import java.nio.file.Path;
 import java.util.concurrent.FutureTask;
 class IoModule {
     public static void main(String[] args) {
         String localPath = "." + File.separator;
         FileOperations fo = new FileOperations(localPath);
+        String searchPath = "D:/Descargas/dependencies/mysql-connector-j-9.1.0/src";
+        FutureTask<List<Path>> futureCallableList = new FutureTask<>(fo.getCallableListOfFiles(searchPath));
+        List<Path> result = null;
+        if(!futureCallableList.isCancelled()) {
+            System.out.println("Running future task");
+            futureCallableList.run();
+        }
         try {
-            fo.getCallableListOfFiles(localPath + "src")
-                .call()
+            if(futureCallableList.isDone()) {
+                System.out.println("Done future task");
+                result = futureCallableList.get();
+            }
+            if(result == null) {
+                throw new Exception("No files found inside | " + searchPath + " |");
+            }
+            result
                 .stream()
                 .forEach(System.out::println);
         } catch(Exception e) {
