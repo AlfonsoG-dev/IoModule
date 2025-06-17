21ae1cba9c251535f80682434fc0d049ed597c7d
AlfonsoG-dev <alfgomachi@gmail.com>
2025-06-17 00:38:54 -0500

    feat(scope): Executor utils
    This class help to maintain a clean and readable implementation of the
    execution of `Runnable` or `Callable` classes, in different ways.
    1. `FutureTask` are used to execute in a single thread.
    2. `ExecutorService` is use to execute in multithread.

---
+++ src/application/ExecutorOperation.java
@@ -0,0 +1,80 @@
+
+import java.util.List;
+import java.util.ArrayList;
+
+import java.nio.file.Path;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutionException;
+
+public class ExecutorOperation {
+    private String localPath;
+    public ExecutorOperation(String localPath) {
+        this.localPath = localPath;
+    }
+
+    public void executeRunnableList(Runnable r, List<Path> result) {
+        FutureTask<List<Path>> futureRunnableTask = new FutureTask<>(r, result);
+
+        if(!futureRunnableTask.isCancelled()) {
+            System.out.println("Starting computation");
+            futureRunnableTask.run();
+        }
+        try {
+            futureRunnableTask.get();
+            System.out.println("Waiting to get the result...");
+            if(futureRunnableTask.isDone()) {
+                System.out.println("Done computation showing results");
+                result
+                    .stream()
+                    .forEach(System.out::println);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void executeCallableList(Callable<List<Path>> c) {
+        FutureTask<List<Path>> futureCallableList = new FutureTask<>(c);
+
+        if(!futureCallableList.isCancelled()) {
+            System.out.println("Starting computation");
+            futureCallableList.run();
+        }
+        try {
+            List<Path> result = futureCallableList.get();
+            System.out.println("Waiting to get the result...");
+            if(futureCallableList.isDone()) {
+                System.out.println("Done computation showing results");
+                result
+                    .stream()
+                    .forEach(System.out::println);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void executorCallableList(Callable<List<Path>> c) {
+        FutureTask<List<Path>> futureCallableList = new FutureTask<>(c);
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(futureCallableList);
+        System.out.println("Starting computation");
+        try {
+            // Esperar a que la tarea se complete
+            List<Path> result = futureCallableList.get();
+            System.out.println("Waiting to get the results...");
+            if(futureCallableList.isDone()) {
+                System.out.println("Done computation showing results");
+                result
+                    .forEach(System.out::println);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
