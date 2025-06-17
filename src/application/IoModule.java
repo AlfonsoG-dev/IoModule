efb04c91c3bba8fc5270feb360c00bec8e1c9b3d
AlfonsoG-dev <alfgomachi@gmail.com>
2025-06-17 00:41:00 -0500

    refactor: use the new implemented class for the runnable and callable execution

--- src/application/IoModule.java
+++ src/application/IoModule.java
@@ -1,41 +1,21 @@
 import java.io.File;
 import java.util.List;
+import java.util.ArrayList;
 import java.nio.file.Path;
-import java.util.concurrent.*;
+import java.nio.file.Paths;
 
 public class IoModule {
     public static void main(String[] args) {
         String localPath = "." + File.separator;
         FileOperations fo = new FileOperations(localPath);
         String searchPath = "D:/Descargas/dependencies/mysql-connector-j-9.1.0/src";
+        List<Path> files = new ArrayList<>();
 
-        // Crear FutureTask
-        FutureTask<List<Path>> futureCallableList = new FutureTask<>(fo.getCallableListOfFiles(searchPath));
+        ExecutorOperation ep = new ExecutorOperation(localPath);
 
-        // Usar un ExecutorService para manejar la ejecución asincrónica
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        executor.submit(futureCallableList);
+        // ep.executeRunnableList(fo.runnableListFiles(Paths.get(searchPath), files), files);
+        // ep.executeCallableList(fo.getCallableListOfFiles(searchPath));
+        // ep.executorCallableList(fo.getCallableListOfFiles(searchPath));
 
-        try {
-            // Esperar a que la tarea se complete
-            List<Path> result = futureCallableList.get();  // Esto bloqueará hasta que el resultado esté disponible
-
-            // Verificar si no hay archivos
-            if (result == null || result.isEmpty()) {
-                throw new IllegalArgumentException("No files found inside | " + searchPath + " |");
-            }
-
-            // Imprimir resultados
-            result.forEach(System.out::println);
-
-        } catch (InterruptedException | ExecutionException e) {
-            System.err.println("Error al ejecutar la tarea: " + e.getMessage());
-            e.printStackTrace();
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } finally {
-            // Asegurarse de que el executor se apague correctamente
-            executor.shutdown();
-        }
     }
 }
