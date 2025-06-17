58d224e7a575af3b51140f30397093f66c7eec2d
AlfonsoG-dev <alfgomachi@gmail.com>
2025-06-17 00:50:29 -0500

    build: update build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$srcClases = ".\src\application\*.java "
+$srcClases = ".\src\application\*.java src\application\*.java src\application\operations\*.java "
 $libFiles = ""
 $compile = "javac -Werror -Xlint:all -d .\bin\ $srcClases"
 $createJar = "jar -cfm IoModule.jar Manifesto.txt -C .\bin\ ."
