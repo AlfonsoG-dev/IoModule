d5d561702f85733732d5c15e09d63911d42f8063
AlfonsoG-dev <alfgomachi@gmail.com>
2025-11-23 21:53:40 -0500

    perf: close IO streams and use lambdas on callable or runnable methods

--- src/application/operations/FileOperations.java
+++ src/application/operations/FileOperations.java
@@ -1,4 +1,4 @@
-package operations;
+package application.operations;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -14,12 +14,13 @@ import java.nio.file.Paths;
 import java.nio.file.FileVisitOption;
 
 import java.util.concurrent.Callable;
+import java.util.stream.Stream;
 
 public class FileOperations {
     public List<Path> getDirectoryFiles(String dirPath) {
         List<Path> files = new ArrayList<>();
-        try {
-            files = Files.walk(Paths.get(dirPath), FileVisitOption.FOLLOW_LINKS)
+        try(Stream<Path> s = Files.walk(Paths.get(dirPath), FileVisitOption.FOLLOW_LINKS)) {
+            files = s
                 .filter(Files::isRegularFile)
                 .toList();
         } catch(Exception e) {
@@ -33,9 +34,9 @@ public class FileOperations {
     }
     public List<File> getDirectoryNames(String dirPath) {
         List<File> names = new ArrayList<>();
-        try {
-            names = Files.walk(Paths.get(dirPath), FileVisitOption.FOLLOW_LINKS)
-                .map(p -> p.toFile())
+        try(Stream<Path> s = Files.walk(Paths.get(dirPath), FileVisitOption.FOLLOW_LINKS)) {
+            names = s
+                .map(Path::toFile)
                 .filter(p -> p.isDirectory() && countFiles(p) > 0)
                 .toList();
         } catch(Exception e) {
@@ -44,30 +45,17 @@ public class FileOperations {
         return names;
     }
     public Callable<List<Path>> getCallableListOfDirNames(String dirPath) {
-        return new Callable<List<Path>>() {
-            public List<Path> call() {
-                return getDirectoryNames(dirPath).stream().map(p -> p.toPath()).toList();
-            }
-        };
+        return () -> getDirectoryNames(dirPath).stream().map(File::toPath).toList();
     }
     public Callable<List<Path>> getCallableListOfFiles(String dirPath) {
-        return new Callable<List<Path>>() {
-            public List<Path> call() {
-                return getDirectoryFiles(dirPath);
-            }
-        };
+        return () -> getDirectoryFiles(dirPath);
     }
     public Runnable runnableListFiles(Path dir, List<Path> files) {
-        return new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Files.walk(dir, FileVisitOption.FOLLOW_LINKS)
-                        .filter(Files::isRegularFile)
-                        .forEach(p -> files.add(p));
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
+        return () -> {
+            try(Stream<Path> s = Files.walk(dir, FileVisitOption.FOLLOW_LINKS)) {
+                files.addAll(s.toList());
+            } catch(Exception e) {
+                e.printStackTrace();
             }
         };
     }
@@ -84,12 +72,7 @@ public class FileOperations {
         return lines;
     }
     public Callable<List<String>> getCallableFileLines(String filePath) {
-        return new Callable<>() {
-            @Override
-            public List<String> call() {
-                return getFileLines(filePath);
-            }
-        };
+        return () -> getFileLines(filePath);
     }
 }
 
